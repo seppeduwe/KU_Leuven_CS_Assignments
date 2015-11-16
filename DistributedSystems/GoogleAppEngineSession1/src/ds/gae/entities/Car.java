a833ac858051a8e6976006842b4ae0aa30d0bfce
Seppe Duwé <seppe@duwe.be>
2015-11-16 22:05:07 +0100

    Initial Assignment GAE

---
+++ DistributedSystems/GoogleAppEngineSession1/src/ds/gae/entities/Car.java
@@ -0,0 +1,67 @@
+package ds.gae.entities;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Car {
+
+    private int id;
+    private CarType type;
+    private Set<Reservation> reservations;
+
+    /***************
+     * CONSTRUCTOR *
+     ***************/
+    
+    public Car(int uid, CarType type) {
+    	this.id = uid;
+        this.type = type;
+        this.reservations = new HashSet<Reservation>();
+    }
+
+    /******
+     * ID *
+     ******/
+    
+    public int getId() {
+    	return id;
+    }
+    
+    /************
+     * CAR TYPE *
+     ************/
+    
+    public CarType getType() {
+        return type;
+    }
+
+    /****************
+     * RESERVATIONS *
+     ****************/
+    
+    public Set<Reservation> getReservations() {
+    	return reservations;
+    }
+
+    public boolean isAvailable(Date start, Date end) {
+        if(!start.before(end))
+            throw new IllegalArgumentException("Illegal given period");
+
+        for(Reservation reservation : reservations) {
+            if(reservation.getEndDate().before(start) || reservation.getStartDate().after(end))
+                continue;
+            return false;
+        }
+        return true;
+    }
+    
+    public void addReservation(Reservation res) {
+        reservations.add(res);
+    }
+    
+    public void removeReservation(Reservation reservation) {
+        // equals-method for Reservation is required!
+        reservations.remove(reservation);
+    }
+}
