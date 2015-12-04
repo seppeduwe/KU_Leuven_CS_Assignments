b41eaf61ce6cbaddd30bb051c01dff8b810ff087
Seppe Duwé <seppe@duwe.be>
2015-12-04 16:06:42 +0100

    Google App Engine Session 2
    Distributed Systems

---
+++ DistributedSystems/GoogleAppEngineSession2/src/ds/gae/ReservationException.java
@@ -0,0 +1,10 @@
+package ds.gae;
+
+public class ReservationException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public ReservationException(String string) {
+        super(string);
+    }
+}
